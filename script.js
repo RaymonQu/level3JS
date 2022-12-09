64fc3954d19f75210d0748616c1fc2ed06c84c3f
Ray <ray>
2022-12-09 14:01:46 -0500

    made thing

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function initialize(){
 function rollDice(){
     var numRolls = getRandomInteger(25, 100);
 }
+
+function getRandomInteger(a, b){
+    
+    return Math.floor((Math.random() * b) + a)
+}
+
+
+console.log(getRandomInteger(25, 100));
